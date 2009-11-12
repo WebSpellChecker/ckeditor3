b812d5ba2059b13cb049b30f6a327d4250f3b8a6
Garry Yao <garry.yao@cksource.com>
2009-11-12 15:15:19 +0000

    Revert wrong changes included in [4470].
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/versions/3.1.x@4471 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- config.js
+++ config.js
@@ -8,5 +8,4 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.contentsCss = 'body { background-color:#fff; color:#222; font-family:Arial, Verdana, sans-serif; font-size:12px; } html { _overflow-y:scroll; } img:-moz-broken { -moz-force-broken-image-icon:1; height:24px; width:24px; } img,input,textarea { cursor:default; }';
 };

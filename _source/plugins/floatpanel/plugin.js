02d66e35e86f696fb2ac83557f94173d6cec84f5
Garry Yao <garry.yao@cksource.com>
2010-01-18 16:52:31 +0000

    Fixing a bug where color button panel has scrollbar in FF.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/features/aria@4943 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/floatpanel/plugin.js
+++ _source/plugins/floatpanel/plugin.js
@@ -246,6 +246,7 @@ CKEDITOR.plugins.add( 'floatpanel',
 						// Set the panel frame focus, so the blur event gets fired.
 						CKEDITOR.tools.setTimeout( function()
 						{
+							iframe.$.contentWindow.focus();
 							// We need this get fired manually because of unfired focus() function.
 							this.allowBlur( true );
 						}, 0, this);

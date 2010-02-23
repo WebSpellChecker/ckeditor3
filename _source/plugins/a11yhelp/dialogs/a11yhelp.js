212bab257a0c5d45f9f670e685830340b03aef87
Frederico Knabben <f.knabben@cksource.com>
2010-02-23 13:50:44 +0000

    Enhancements to the accessibility instructions dialog.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/versions/3.2.x@5172 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/a11yhelp/dialogs/a11yhelp.js
+++ _source/plugins/a11yhelp/dialogs/a11yhelp.js
@@ -5,7 +5,8 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.dialog.add( 'a11yHelp', function( editor )
 {
-	var lang = editor.lang.accessibilityHelp;
+	var lang = editor.lang.accessibilityHelp,
+		id = CKEDITOR.tools.getNextNumber();
 
 	// CharCode <-> KeyChar.
 	var keyMap =
@@ -124,7 +125,8 @@ CKEDITOR.dialog.add( 'a11yHelp', function( editor )
 	// Create the help list directly from lang file entries.
 	function buildHelpContents()
 	{
-		var pageTpl = '<div class="cke_accessibility_legend" tabIndex="-1">%1</div>',
+		var pageTpl = '<div class="cke_accessibility_legend" role="document" aria-labelledby="cke_' + id + '_arialbl" tabIndex="-1">%1</div>' +
+				'<span id="cke_' + id + '_arialbl" class="cke_voice_label">' + lang.contents + ' </span>',
 			sectionTpl = '<h1>%1</h1><dl>%2</dl>',
 			itemTpl = '<dt>%1</dt><dd>%2</dd>';
 

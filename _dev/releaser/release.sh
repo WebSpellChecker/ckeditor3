a1f0f19299893a65fb1767e8fe856c9bcbbc0829
Frederico Knabben <f.knabben@cksource.com>
2010-08-13 17:51:22 +0000

    Updated the release version.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/versions/3.4.x@5825 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.sh
+++ _dev/releaser/release.sh
@@ -12,5 +12,5 @@ fi ;
 LANGTOOL="$(cd $(dirname "$0"); pwd)/langtool.sh"
 
 pushd $DIR
-java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.4 Beta" ckeditor_3.4b --run-before-release=$LANGTOOL
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.4" ckeditor_3.4 --run-before-release=$LANGTOOL
 popd

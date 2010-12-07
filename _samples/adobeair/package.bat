ce69e2cff07541ec8adbf5b93e9883d51736c146
Frederico Knabben <f.knabben@cksource.com>
2010-12-07 00:58:05 +0000

    Minor changes to the AIR sample.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/features/adobeair@6177 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _samples/adobeair/package.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-::
-:: Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
-:: For licensing, see LICENSE.html or http://ckeditor.com/license
-::
-
-adt -package -storetype pkcs12 -keystore cert.pfx -storepass 123abc CKEditor.air application.xml -C ../../ .

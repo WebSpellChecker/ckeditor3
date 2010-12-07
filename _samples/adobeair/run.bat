0deb96bad0b93fff8723607785b8165af2c2963a
Frederico Knabben <f.knabben@cksource.com>
2010-12-07 10:28:54 +0000

    Fixed the AIR sample.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/versions/3.5.x@6179 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _samples/adobeair/run.bat
+++ _samples/adobeair/run.bat
@@ -3,10 +3,10 @@
 :: Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 :: For licensing, see LICENSE.html or http://ckeditor.com/license
 ::
-:: Use this file to quickly run the sample in a Windows environment.
+:: Use this file to quickly run the sample in a Windows environment.
 ::
 :: Be sure to change the following directory path so it matches your AIR SDK
 :: installation path.
 ::
 
-D:/AdobeAIRSDK/bin/adl application.xml ../../
+C:/AdobeAIRSDK/bin/adl application.xml ../../

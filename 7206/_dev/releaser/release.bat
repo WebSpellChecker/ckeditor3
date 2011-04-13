63aa73f10bf0844b314dca4905912befc4f489cb
Frederico Knabben <f.knabben@cksource.com>
2011-04-13 15:28:20 +0000

    Created a branch for #7206 to help speed up the research.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/developers/ibm@6673 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ 7206/_dev/releaser/release.bat
@@ -0,0 +1,13 @@
+::
+:: Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+:: For licensing, see LICENSE.html or http://ckeditor.com/license
+::
+
+@ECHO OFF
+
+CLS
+ECHO.
+
+:: rmdir /S /Q release
+
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.5.4 (SVN)" ckeditor_3.5.4_svn --run-before-release=langtool.bat

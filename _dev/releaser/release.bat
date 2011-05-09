e1943da717da1888d4e9c585caa170aabb99b88e
Wiktor Walc <w.walc@cksource.com>
2011-05-09 15:03:55 +0000

    Closed the 3.6 release.
    git-svn-id: http://svn.ckeditor.com/CKEditor/branches/versions/3.6.x@6903 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.bat
+++ _dev/releaser/release.bat
@@ -10,4 +10,4 @@ ECHO.
 
 :: rmdir /S /Q release
 
-java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.6 (SVN)" ckeditor_3.6_svn --run-before-release=langtool.bat
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.6" ckeditor_3.6 --run-before-release=langtool.bat

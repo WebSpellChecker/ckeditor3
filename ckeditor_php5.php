1e8dd79c0956fed55b2d0b8147e1e53efe2696bc
Wiktor Walc <w.walc@cksource.com>
2012-04-17 11:19:04 +0000

    Updated the releases branch with CKEditor 3.6.3
    git-svn-id: http://svn.ckeditor.com/CKEditor/releases/latest@7476 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- ckeditor_php5.php
+++ ckeditor_php5.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+* Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 * For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
@@ -24,7 +24,7 @@ class CKEditor
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'C3GK5RI';
+	const timestamp = 'C3HA5RM';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "C3GK5RI";
+	public $timestamp = "C3HA5RM";
 	/**
 	 * An array that holds event listeners.
 	 */

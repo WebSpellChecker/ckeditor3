ce68afdd84807e2837ef0773cea22fcbbfbbdef1
Wiktor Walc <w.walc@cksource.com>
2013-02-20 16:21:02 +0000

    Reverted [7693]
    git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7694 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _samples/assets/_posteddata.php
+++ _samples/assets/_posteddata.php
@@ -25,24 +25,24 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 		</thead>
 <?php
 
-if (!empty($_POST))
-{
-	foreach ( $_POST as $key => $value )
-	{
-		if ( ( !is_string($value) && !is_numeric($value) ) || !is_string($key) )
-			continue;
+if ( isset( $_POST ) )
+	$postArray = &$_POST ;			// 4.1.0 or later, use $_POST
+else
+	$postArray = &$HTTP_POST_VARS ;	// prior to 4.1.0, use HTTP_POST_VARS
 
-		if ( get_magic_quotes_gpc() )
-			$value = htmlspecialchars( stripslashes((string)$value) );
-		else
-			$value = htmlspecialchars( (string)$value );
+foreach ( $postArray as $sForm => $value )
+{
+	if ( get_magic_quotes_gpc() )
+		$postedValue = htmlspecialchars( stripslashes( $value ) ) ;
+	else
+		$postedValue = htmlspecialchars( $value ) ;
+
 ?>
 		<tr>
-			<th style="vertical-align: top"><?php echo htmlspecialchars( (string)$key ); ?></th>
-			<td><pre class="samples"><?php echo $value; ?></pre></td>
+			<th style="vertical-align: top"><?php echo htmlspecialchars($sForm); ?></th>
+			<td><pre class="samples"><?php echo $postedValue?></pre></td>
 		</tr>
 	<?php
-	}
 }
 ?>
 	</table>
